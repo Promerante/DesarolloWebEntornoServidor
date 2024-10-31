0bfc8b9c136885df94b0d32b8a5be4faad78c17f
Facu <facundodanielbenitez@gmail.com>
2024-10-31 20:42:54 +0100

    clase 31/10

--- Practicas/Tema1A5/Examen/ejercicios+.php
+++ Practicas/Tema1A5/Examen/ejercicios+.php
@@ -17,10 +17,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         }
         echo "<pre>";
-        print_r($pascal);
+        print_r(mostrarPascalInvertida($pascal));
         echo "</pre>";
+        mostrarPascalInvertida($pascal);
     }
 }
+    function mostrarPascalInvertida($pascal){
+        //Primero invertimos el orden
+        $pascalInv=[];
+        $cont=0;
+        for($i=count($pascal)-1;$i>=0;$i--){
+            $pascalInv[$cont]=$pascal[$i];
+            $cont++;
+        }
+        //Ahora lo rellenamos de 0;
+        for($i=0;$i<count($pascalInv);$i++){
+            while(count($pascalInv[$i])!=count($pascalInv[0]))
+                array_unshift($pascalInv[$i],0);
+        }
+        //Y ahora lo imprimimos
+        for($i=0;$i<count($pascalInv);$i++){
+            echo "&nbsp;&nbsp;";
+            for($j=0;$j<count($pascalInv[$i]);$j++){
+                if($pascalInv[$i][$j]==0){
+                    echo "&nbsp;&nbsp;";
+                }
+                else{
+                    echo $pascalInv[$i][$j]."&nbsp;&nbsp;";
+                }
+            }
+            echo "<br>";
+        }
+        return $pascalInv;
+    }
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $value = $_POST['form'];
     if ($value == 2) {
@@ -61,3 +91,4 @@ function esPrimo($num)
     }
     return true;
 }
+
