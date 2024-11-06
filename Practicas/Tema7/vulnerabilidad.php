64ba08c4c70952d79087bb0ea6cd2d0f139853e8
Facu <facundodanielbenitez@gmail.com>
2024-11-06 18:34:11 +0100

    6/11

---
+++ Practicas/Tema7/vulnerabilidad.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vulnerabilidad</title>
+</head>
+<body>
+<form action="" method="post">
+    <input type="text" name="comentario">
+    <input type="submit" value="Enviar">
+</form>    
+<!--<script>alert("Has sido hackeado");
+    document.body.innerHTML="<h1>Sitio hackiado</h1>"
+</script>-->
+<form action="" method="get">
+    <input type="text" name="comentario">
+    <input type="submit" value="Enviar">
+</form>
+</body>
+</html>
+<?php
+    error_reporting(E_ALL);
+    ini_set("display_errors",1);
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $comentario=$_POST["comentario"];
+    echo $comentario;
+}
+if($_SERVER["REQUEST_METHOD"]=="GET"){
+    $comentario=htmlspecialchars($_GET["comentario"]);
+    
+}
+isset($pepe);
+empty($pepe);
+?>
